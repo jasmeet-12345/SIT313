d33224eda4b9f4be95dc97a95938f7ca03edc0b2
Jasmeet Kaur <jasmeet4813.se24@chitkara.edu.in>
2025-09-04 15:17:33 +0530

    Add files via upload

---
+++ Task 5.1C/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+import NewPost from "./NewPost";
+
+function App() {
+  return( 
+  <div> 
+  <Header />
+  <NewPost />
+  </div>
+  );
+}
+
+export default App;
